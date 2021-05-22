4bdec2d177fc9b6abf951d22720e145f07b14691
Pratik <pratik.shinde@cdk.com>
2021-05-23 00:13:03 +0530

    Can operate with multiple pincodes, specific dose

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # configurations
-pin_code = '413701' # Enter your pin code
-refresh_time = 300  # Time in seconds to refresh the slot availibility
-days = 10           # Number of days you want to check slots for
-age = [18, 45]      # Keep the age limit you are looking for
+pin_codes = ['411028']  # Enter pin codes
+refresh_time = 30       # Time in seconds to refresh the slot availibility
+days = 10               # Number of days you want to check slots for
+age = [18]              # 18 for 18-44 | 45 for 45 +
+dose = [1]              # Dose 1 / Dose 2
--- cowin_slots.py
+++ cowin_slots.py
@@ -6,26 +6,29 @@ from os import system
 from beepy import beep
 import config
 
-pin_code = config.pin_code
+pin_codes = config.pin_codes
 refresh_time = config.refresh_time
 days = config.days
 age = config.age
+doses = config.dose
 
 
-def print_report(session_details, center_details):
-        system('clear')
-        print(f"Center Id           :{center_details['center_id']}\n"
-              f"Name                :{center_details['name']}\n"
-              f"Address             :{center_details['address']}\n"
-              f"Date                :{session_details['date']}\n"
-              f"Available capacity  :{session_details['available_capacity']}\n"
-              f"Min age limit       :{session_details['min_age_limit']}\n"
-              f"Vaccine             :{session_details['vaccine']}")
-        print('\nClick here https://www.cowin.gov.in/home to book your appointment')
-        while True: beep(sound=4)
+def print_report(session_details, center_details, dose, pin_code):
+    capacity = session_details['available_capacity_dose1'] if dose == 1 else session_details['available_capacity_dose2']
+    system('clear')
+    print(f"Center Id           :{center_details['center_id']}\n"
+          f"Name                :{center_details['name']}\n"
+          f"Address             :{center_details['address']}\n"
+          f"Pin Code            :{pin_code}\n"
+          f"Date                :{session_details['date']}\n"
+          f"Available capacity  :{capacity}, Dose: {dose}\n"
+          f"Min age limit       :{session_details['min_age_limit']}\n"
+          f"Vaccine             :{session_details['vaccine']}")
+    print('\nClick here https://www.cowin.gov.in/home to book your appointment')
+    while True: beep(sound=4)
 
 
-def check_slot(dates):
+def check_slot(dates, pin_code):
     date_param = ','.join(dates)
     try:
         conn = http.client.HTTPSConnection("cdn-api.co-vin.in")
@@ -34,16 +37,18 @@ def check_slot(dates):
         if len(response['centers']) > 0:
             for center in response['centers']:
                 for session in center['sessions']:
-                    print(f"\t{session['date']} :", end=' ')
                     if session['min_age_limit'] in age:
-                        if session['available_capacity'] > 0:
-                            print_report(session, center)
-                        else:
-                            print(f"Center already booked   >> {center['name']}")
+                        for dose in doses:
+                            if dose == 1 and session["available_capacity_dose1"] > 0:
+                                print_report(session, center, dose, pin_code)
+                            elif dose == 2 and session["available_capacity_dose2"] > 0:
+                                print_report(session, center, dose, pin_code)
+                            else:
+                                print(f"\t{session['date']} : Center already booked   >> {center['name']}")
                     else:
-                        print(f"Not available for {age[0]}    >> {center['name']}")
+                        print(f"\t{session['date']} : Not available for {age[0]}+   >> {center['name']}")
         else:
-            print(f'\tNo centers available for any requested date')
+            print(f'\tNo centers available at {pin_code} for {len(days)} upcoming days')
     except:
         print('\tCowin API is not reachable right now')
 
@@ -51,8 +56,9 @@ def check_slot(dates):
 days = [(datetime.datetime.now() + datetime.timedelta(days=day)).strftime('%d-%m-%Y') for day in range(days)]
 iterations = 1
 while True:
+    system('clear')
     print(f'Iteration: {iterations}, {datetime.datetime.now().strftime("%d-%m-%Y %I:%M %p")}')
-    check_slot(days)
+    for pincode in pin_codes:check_slot(days, pincode)
     iterations += 1
     while refresh_time > 0: print(f'Checking again in {refresh_time} seconds..', end='\r'); refresh_time -= 1; time.sleep(1)
     refresh_time = config.refresh_time
